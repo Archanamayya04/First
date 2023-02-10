bbdf033597f5a992c480b2243e1025233ab43bf2
Archanamayya04 <124157899+Archanamayya04@users.noreply.github.com>
2023-02-10 18:21:32 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -7,6 +7,13 @@ class Sample
     int c=a+b;
     Sustem.out.println(c);
   }
+  static void add()
+  {
+    int a=10;
+    int b=5;
+    int c=a+b;
+    Sustem.out.println(c);
+  }
   public static void main(String[] args)
   {
     add();
